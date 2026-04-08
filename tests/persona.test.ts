767647f4210c1c069956f24cee4f0114685dbd48
CodermatzValentino09 <valencodermatz@gmail.com>
2026-04-08 01:05:17 -0300

    Mejora de cobertura de tests en Persona
    Agrego tests para validar getNombre y getDni en Persona

--- tests/persona.test.ts
+++ tests/persona.test.ts
@@ -8,4 +8,25 @@ describe("Clase Persona", () => {
     expect(persona).toBeInstanceOf(Persona);
   });
 
+  import { Persona } from "../src/Persona";
+
+describe("Clase Persona", () => {
+
+  test("crea una persona correctamente", () => {
+    const persona = new Persona("Juan", 12345678);
+    expect(persona).toBeInstanceOf(Persona);
+  });
+
+  test("obtiene el nombre correctamente", () => {
+    const persona = new Persona("Juan", 12345678);
+    expect(persona.getNombre()).toBe("Juan");
+  });
+
+  test("obtiene el dni correctamente", () => {
+    const persona = new Persona("Juan", 12345678);
+    expect(persona.getDni()).toBe(12345678);
+  });
+
+});
+
 });
